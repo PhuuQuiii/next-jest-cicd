ef9d2012c4b27d908c6c6f29a3def3f8812e8c46
PhuuQuiii <nguyenphuqui170304@gmail.com>
2025-04-06 23:59:54 +0700

    change nội dung trong page

--- app/page.test.tsx
+++ app/page.test.tsx
@@ -8,7 +8,7 @@ import Page from "./page";
 jest.mock("./page", () => ({
   __esModule: true,
   metadata: {},
-  default: jest.fn(() => <h1>App Router</h1>),
+  default: jest.fn(() => <h1>Phú Quí tập làm CI/CD</h1>),
 }));
 
 it("App Router: Works with Server Components", () => {
