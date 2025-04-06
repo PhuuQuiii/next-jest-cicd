f834f8988137a48dad5d5f9362aa65eb7ff3af7f
PhuuQuiii <nguyenphuqui170304@gmail.com>
2025-04-06 22:11:32 +0700

    update npm test

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -4,10 +4,15 @@ type Params = {
   };
 };
 
-export async function generateMetadata({ params }: Params) {
+export async function generateMetadata({ params }: { params: { slug: string } }) {
   return { title: `Post: ${params.slug}` };
 }
 
-export default function Page({ params }: Params) {
-  return <><h1>Slug: {params.slug}</h1><p>HoleTex</p></>
+// export default function Page({ params }: { params: { slug: string } })  {
+//   return <><h1>Slug: {params.slug}</h1><p>HoleTex</p><p>Welcom to HCM</p></>
+// }
+
+
+export default function Page({ params }: { params: { slug: string } }) {
+  return <h1>Slug: {params.slug}</h1>;
 }
